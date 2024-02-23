237357a5171e7628e36606b4fa818717b78ff6aa
Julian-Belza <145355671+Julian-Belza@users.noreply.github.com>
2024-02-23 08:30:36 -0500

    doing a bunch of front end work rn

--- Assets/Scripts/MusicToPlay.cs
+++ Assets/Scripts/MusicToPlay.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class MusicToPlay : MonoBehaviour
 {
+    public GameObject GameMusic;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   void Awake()
+   {
 
-    // Update is called once per frame
-    void Update()
+    GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
+    if (musicObj.Length > 1)
     {
-        
+        Destroy(this.gameObject);
     }
+    DontDestroyOnLoad(this.gameObject);
+   }
 }
