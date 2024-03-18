560d901f4ba55df4d0e46fb4bdac7209b5d79efe
Slomar725 <ojimenezd725@gmail.com>
2024-03-17 23:25:02 -0400

    ALpha Done

---
+++ Assets/Scripts/Animation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+
+{
+    public Animator animator;
+    
+    public static Animation instance;
+    public bool isDamage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        isDamage = false;
+
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isDamage = false;
+
+        if(Input.anyKeyDown && !isDamage)
+        {
+            animator.Play("PlayerAnim");
+            
+        }
+    }
+    public void noDamage()
+        {
+            isDamage = true;
+            animator.Play("takenDamage");
+        }
+    
+}
